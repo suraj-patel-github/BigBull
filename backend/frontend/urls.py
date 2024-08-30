a784713b1b2a48aa7697a1d91a6c431d53e1f3eb
abc <patelsuraj301@gmail.com>
2024-08-30 23:35:21 +0530

    final commit for the frontend

--- backend/frontend/urls.py
+++ backend/frontend/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('/login', views.index),
-    path('/register', views.index),
-    path('/dashboard', views.index),
+    path('login', views.index),
+    path('register', views.index),
+    path('dashboard', views.index),
 ]
